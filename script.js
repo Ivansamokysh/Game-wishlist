b6e3ec65f106c09781fc96bb9ad7760daf648938
IvanSamokysh <vanyasam25@gmail.com>
2026-09-04 20:26:07 +0300

    js problems

--- script.js
+++ script.js
@@ -1,8 +1,123 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const API_KEY = '250ff70572f54c47ba15fc8fa203da58';
+    const URL = `https://api.rawg.io/api/games?key=${API_KEY}&search=${SearchQuery}&genres=${genres.join(',')}&platforms=${platforms.join(',')}&sort=${sortBy}`;
+
+    const gamesContainer = document.querySelector('.games-container');
+    const searchInput = document.querySelector('.search-input');
+    const genreCheckboxes = document.querySelectorAll('.genre-checkbox');
+    const platformCheckboxes = document.querySelectorAll('.platform-checkbox');
+    const sortSelect = document.getElementById('sort-select');
+
+    let searchTimeot = null;
+
   const loginBtn = document.querySelector('.btn-login');
   const signupBtn = document.querySelector('.btn-signup');
   const loginModal = document.getElementById('loginModal');
   const signupModal = document.getElementById('signupModal');
+
+  async function GamesFilter(SearchQuery = '', genres = [], platforms = [], sortBy = 'popular') {
+    if (!gamesContainer) return;
+
+    gamesContainer.innerHTML = '<p>Loading...</p>';
+
+    const params = new URLSearchParams({
+        key: API_KEY,
+        page_size: 12
+    });
+
+    if (searchQuery.trim() !== '') {
+        params.append('search', searchQuery.trim());
+    }
+
+    if (genres.length > 0) {
+        params.append('genres', genres.join(',').toLowerCase());
+    }
+
+    if (platforms.length > 0) {
+        params.append('platforms', platforms.join(','));
+    }
+
+    if (sortBy === 'rating') {
+        params.append('ordering', '-rating')
+    } else if (sortBy === 'release-date') {
+        params.append('ordering', '-released')
+    } else if (sortBy === 'name') {
+        params.append('ordering', '-name')
+    } else {
+        params.append('ordering', '-metacritic')
+    }  
+
+    try {
+        const response = await fetch(`${URL}&${params.toString()}`);
+
+        if (!response.ok) {
+            throw new Error(`Помилка запиту:${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.results;
+    } catch (error) {
+        console.error('Помилка при отриманні даних:', error);
+        return [];
+    }
+  }
+
+  function renderGames(games) {
+    gamesContainer.innerHTML = '<p> style="color: #94a1b2;">Ігор за вашим запитом не знайдено.</p>';
+    return;
+
+    games.forEach(game => {
+        const gameCard = document.createElement('div');
+        gameCard.className = 'game-card';
+
+        const coverImage = game.background_image
+        ? game.background_image
+        : 'https://via.placeholder.com/300x400?text=No+Image';
+
+        const mainPlatform = game.game.platforms && game.platforms.length > 0
+        ? game.platforms[0].platform.name
+        : 'N/A';
+
+        const mainGenre = game.genres && game.genres.length > 0
+        ? game.genres[0].name
+        : 'N/A';
+
+        gameCard.innerHTML = `
+            <img src="${coverImage}" alt="${game.name}">
+            <div class="game-info">
+                <h3>${game.name}</h3>
+                <p>Platform: ${mainPlatform}</p>
+                <span class="genre-tag">${mainGenre}</span>
+            </div>
+        `;    
+  })}
+
+    if (searchInput) {
+        searchInput.addEventListener('input', () => {
+            clearTimeout(searchTimeot);
+            searchTimeout = setTimeout(() => {
+                fetchGamesFromApi();
+            }, 500);
+        });
+    }
+
+    if (sortSelect) {
+        sortSelect.addEventListener('change', () => {
+            fetchGamesFromApi();
+    })};
+
+    genreCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            fetchGamesFromApi();
+        });
+    });
+    platformCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            fetchGamesFromApi();
+    }); 
+
+    fetchGamesFromApi();
 
   const userProfile = document.getElementById('userProfile');
   const userName = document.getElementById('userName');
